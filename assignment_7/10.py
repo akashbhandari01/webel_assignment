fb34faa95574fcf75cc71c02f727fd2678d4e167
Akash Bhandari <85107303+akashbhandari01@users.noreply.github.com>
2022-09-23 21:14:27 +0530

    Update 10.py

--- assignment_7/10.py
+++ assignment_7/10.py
@@ -1,6 +1,4 @@
-test_str = "GeeksforGeeks"
-count = 0 
-for i in test_str:
-    if i == 'e':
-        count = count + 1
-print ("Count of e in GeeksforGeeks is : " +  str(count))
+str=input("Enter a String with a pattern: ")
+pattern=input("Enter a Pattern: ")
+result=str.count(pattern)
+print(result)

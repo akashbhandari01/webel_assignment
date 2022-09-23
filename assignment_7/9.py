e892783bf752f91cd7a7cd7cc642315711569500
Akash Bhandari <85107303+akashbhandari01@users.noreply.github.com>
2022-09-23 21:02:49 +0530

    Update 9.py

--- assignment_7/9.py
+++ assignment_7/9.py
@@ -1,9 +1,7 @@
-def matchPat(str1,str2):
-    if str1=='':
-        return True
-    elif str2=='':
-        return False
-    elif str1[0]==str2[0]:
-        return matchPat(str1[2],str2[len(str1)-1])
-    else: return True
-
+str=input("Enter a string: ")
+parttern=input("Enter The Pattern:")
+for parttern in str:
+    print("Pattern Found.")
+    break
+else:
+    print("Pattern not Found.")
